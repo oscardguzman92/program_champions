1175cff1597b8232717d7ab10ba0f95229f984f6
Oscar Daniel Guzman <oscar.guzman@tiendapp.net>
2022-04-21 23:15:33 -0500

    migracion de tabla de matches

---
+++ database/migrations/2022_04_22_040029_create_matches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->string('api_id');
+            $table->date('fecha');
+            $table->string('etapa');
+            $table->string('equipo_local');
+            $table->string('equipo_visitante');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
