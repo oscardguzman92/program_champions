c102812365e25fc101b6a743034eefdcc894117e
Oscar Daniel Guzman <oscar.guzman@tiendapp.net>
2022-04-23 00:33:13 -0500

    configuracion de docker con tres contenedores de servicios: app con php 7.4 fpm, db con mysql 5.7 y nginx para analizar el codigo php de l servicio app, utilizando volumenes compartidos

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
-
+Route::get('/', [App\Http\Controllers\MatchController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\MatchController::class, 'index'])->name('home');
